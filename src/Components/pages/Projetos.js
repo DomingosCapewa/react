475f0c498f14fdf81fd90a148f10c0fb40efdfc6
Domingos <domingoscapewa08@gmail.com>
2024-07-11 13:17:41 -0300

    crr

--- src/Components/pages/Projetos.js
+++ src/Components/pages/Projetos.js
@@ -1,14 +1,25 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 
+import styles from '../layout/Projetos.module.css'                 
+import Container from './../layout/Container';
+import LinkButton from "../layout/LinkButton";
+
 function Projetos() {
     const location = useLocation();
     const message = location.state?.message;
 
     return (
-        <div>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
             <h1>Meus Projetos</h1>
+            <LinkButton to="/novoprojeto" text="Criar projeto" />
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container custonClass="start">
+                <p>Projetos...</p>
+                
+            </Container>
         </div>
     );
 }
