1423202867c7baf9f89fcb759626330a6b2721bd
Domingos <domingoscapewa08@gmail.com>
2024-07-12 16:41:17 -0300

    Adicionado a opção de remover projeto

--- src/Components/pages/Projetos.js
+++ src/Components/pages/Projetos.js
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 
-import styles from '../layout/Projetos.module.css'                 
+import styles from '../layout/Projetos.module.css';                 
 import Container from './../layout/Container';
 import Loading from "../layout/Loading";
 import LinkButton from "../layout/LinkButton";
@@ -9,51 +9,73 @@ import ProjectCard from "../project/ProjectCard";
 import { useState, useEffect } from "react";
 
 function Projetos() {
-    const [projects, setProjects] = useState([])
-    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
+    const [message, setMessage] = useState('');
 
     const location = useLocation();
-    const message = location.state?.message;
+    const initialMessage = location.state?.message;
 
-useEffect(()=> {
-   setTimeout(
-   () => {
-    fetch("http://localhost:5000/projects",
-        {method: "GET",
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:5000/projects", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data);
+                    setProjects(data);
+                    setRemoveLoading(true);
+                })
+                .catch((err) => console.log(err));
+        }, 400);
+    }, []);
+
+    function removeProjeto(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
             headers: {
-               "Content-Type": "application/json"
+                'Content-Type': 'application/json'
             }
         })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            setProjects(data)
-            setRemoveLoading(true)
-         })
-         .catch((err) => console.log(err))
-   }, 500)
-}, [])
+            .then(resp => resp.json())
+            .then(() => {
+                setProjects(projects.filter((project) => project.id !== id));
+                setMessage('Projeto removido com sucesso!');
+            })
+            .catch(err => {
+                console.log(err);
+                setMessage('Erro ao remover o projeto.');
+            });
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
-            <h1>Meus Projetos</h1>
-            <LinkButton to="/novoprojeto" text="Criar projeto" />
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/novoprojeto" text="Criar projeto" />
             </div>
+            {initialMessage && <Message type="success" msg={initialMessage} />}
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-            {projects.length > 0 &&
-    projects.map((project) => (
-        <ProjectCard id={project.id} name={project.name}
-        budget={project.budget}
-        category={project.category.name}
-        key={project.id}/>
-        
-    ))}
-
-      {!removeLoading && <Loading />}  
-      {removeLoading && projects.length === 0  && (
-        <p>Não há projetos cadastrados</p>
-      )}      
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                            handleRemove={() => removeProjeto(project.id)}
+                        />
+                    ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados</p>
+                )}
             </Container>
         </div>
     );
--- src/Components/project/ProjectCard.js
+++ src/Components/project/ProjectCard.js
@@ -5,23 +5,29 @@ import { BsPencil, BsFillTrashFill } from "react-icons/bs"
 
 
 function ProjectCard({id, name, budget, category, handleRemove}) {
-    return(
-        <div className={styles.project_card}>
-            <h4>{name}</h4>
-        <span>Orçamento:</span>R${budget}
-        <p className={styles.category_text}>
-            <span className={`${styles[category.toLowerCase()]}`}></span>{category}
-        </p>
-        <div className={styles.project_card_actions}>
-            <Link to="/">
-            <BsPencil />Editar
-            </Link>
-            <button>
-                <BsFillTrashFill />Excluir
-            </button>
-        </div>
-        </div>
-    )
-}
 
-export default ProjectCard
+        const remove = (e) => {
+            e.preventDefault()
+            handleRemove(id)
+        }
+
+        return(
+            <div className={styles.project_card}>
+                <h4>{name}</h4>
+            <span>Orçamento:</span>R${budget}
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span>{category}
+            </p>
+            <div className={styles.project_card_actions}>
+                <Link to="/">
+                    <BsPencil />Editar
+                </Link>
+                <button onClick={remove}>
+                    <BsFillTrashFill />Excluir
+                </button>
+            </div>
+            </div>
+        )
+    }
+
+    export default ProjectCard
